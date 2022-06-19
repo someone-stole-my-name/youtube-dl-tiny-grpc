5110075bea2ab1c2fc611c1f447d977737801228
Christian Segundo <36006540+someone-stole-my-name@users.noreply.github.com>
2022-06-19 19:51:03 +0200

    Read options from env (#3)

--- youtube_dl_tiny_grpc/options.py
+++ youtube_dl_tiny_grpc/options.py
@@ -1,6 +1,6 @@
 import argparse
 from multiprocessing import cpu_count
-from os import environ
+import os
 
 from setproctitle import getproctitle
 
@@ -23,20 +23,21 @@ def parse_opts(argv: list) -> argparse.Namespace:
 
     prometheus.add_argument(
         '--prometheus-port',
-        default=8080,
+        default=os.getenv("PROMETHEUS_PORT", 8080),
         type=int,
         help='Port to listen on',
     )
 
     prometheus.add_argument(
         '--prometheus-enable',
+        default=os.getenv("PROMETHEUS_ENABLE", False),
         action='store_true',
         help='Enable Prometheus metrics',
     )
 
     prometheus.add_argument(
         '--prometheus-directory',
-        default="/tmp/youtube_dl_tiny_grpc_prometheus",
+        default=os.getenv("PROMETHEUS_DIRECTORY", "/tmp/youtube_dl_tiny_grpc_prometheus"),
         type=str,
         help='Prometheus directory to share between processes',
     )
@@ -45,27 +46,27 @@ def parse_opts(argv: list) -> argparse.Namespace:
 
     grpc.add_argument(
         '--grpc-port',
-        default=50051,
+        default=os.getenv("GRPC_PORT", 50051),
         type=int,
         help='Port to listen on',
     )
 
     grpc.add_argument(
         '--grpc-graceful-shutdown-timeout',
-        default=60,
+        default=os.getenv("GRPC_GRACEFUL_SHUTDOWN_TIMEOUT", 60),
         type=int,
         help='Graceful shutdown timeout',
     )
 
     grpc.add_argument(
         '--grpc-no-reflection',
-        action='store_true',
+        default=os.getenv("GRPC_NO_REFLECTION", False),
         help='Disable gRPC server reflection',
     )
 
     grpc.add_argument(
         '--grpc-compression-algorithm',
-        default='gzip',
+        default=os.getenv("GRPC_COMPRESSION_ALGORITHM", "gzip"),
         choices=_COMPRESSION_ALGORITHMS,
         help='Compression algorithm for the server',
     )
@@ -74,26 +75,28 @@ def parse_opts(argv: list) -> argparse.Namespace:
 
     youtube_dl.add_argument(
         '--youtube-dl-max-workers',
-        default=cpu_count(),
+        default=os.getenv("YOUTUBE_DL_MAX_WORKERS", cpu_count()),
         type=int,
         help='Max number of workers to use for youtube-dl',
     )
 
     youtube_dl.add_argument(
         '--youtube-dl-verbose',
+        default=os.getenv("YOUTUBE_DL_VERBOSE", False),
         action='store_true',
         help='Verbose output for youtube-dl',
     )
 
     youtube_dl.add_argument(
         '--youtube-dl-no-quiet',
+        default=os.getenv("YOUTUBE_DL_NO_QUIET", True),
         action='store_false',
         help='Disable quiet output for youtube-dl',
     )
 
     youtube_dl.add_argument(
         '--youtube-dl-proxy-list',
-        default="",
+        default=os.getenv("YOUTUBE_DL_PROXY_LIST", ""),
         type=str,
         help="Comma separated list of proxies to use. \
         For example 'socks5://127.0.0.1:1080,http://127.0.0.1:8080'"
@@ -101,7 +104,7 @@ def parse_opts(argv: list) -> argparse.Namespace:
 
     youtube_dl.add_argument(
         '--youtube-dl-cookies-file',
-        default="",
+        default=os.getenv("YOUTUBE_DL_COOKIES_FILE", ""),
         type=str,
         help="File to read cookies from and dump cookie jar in"
     )
@@ -111,20 +114,21 @@ def parse_opts(argv: list) -> argparse.Namespace:
     redis.add_argument(
         '--redis-enable',
         action='store_true',
+        default=os.getenv("REDIS_ENABLE", False),
         help='Enable Redis cache support',
     )
 
     redis.add_argument(
         '--redis-uri',
         type=str,
-        default='redis://localhost:6379',
+        default=os.getenv("REDIS_URI", 'redis://localhost:6379'),
         help='Redis URI to connect to',
     )
 
     redis.add_argument(
         '--redis-ttl',
         type=int,
-        default=3600,
+        default=os.getenv("REDIS_TTL", 3600),
         help='TTL for cached results',
     )
 
@@ -139,18 +143,20 @@ def parse_opts(argv: list) -> argparse.Namespace:
     general.add_argument(
         '--debug',
         action='store_true',
+        default=os.getenv("DEBUG", False),
         help='Enable debug logging',
     )
 
     general.add_argument(
         '--verbose',
         action='store_true',
+        default=os.getenv("VERBOSE", False),
         help='Enable verbose logging',
     )
 
     args = parser.parse_args(argv)
 
-    if args.verbose or args.debug or environ.get("DEBUG") is not None:
+    if args.verbose or args.debug is not None:
         print("configuration:")
         for arg in vars(args):
             print(f"  {arg}: {getattr(args, arg)}")
